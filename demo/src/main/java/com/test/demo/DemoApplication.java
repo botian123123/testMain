a1b92c1a4de2cd7e0189deee10cd01460bfe2cca
botian123123 <bo.tian@pax.us>
2024-01-17 14:43:06 -0500

    add test script

--- demo/src/main/java/com/test/demo/DemoApplication.java
+++ demo/src/main/java/com/test/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.test.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.cicdtest.model.Model;
+
+import main.java.com.example.sub2.Modle2;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Autowired
+	Model person;
+	@Autowired
+	Modle2 car;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner test() {
+		return args -> {
+			person.speak();
+			System.out.println(" driving " + car.getName() + " " + car.getType());
+		};
+	}
 }
---
+++ demo/src/main/java/com/test/demo/config/appConfig.java
@@ -0,0 +1,20 @@
+package com.test.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.cicdtest.model.Model;
+
+import main.java.com.example.sub2.Modle2;
+
+@Configuration
+public class appConfig {
+    @Bean
+    public Model getPerson() {
+        return new Model();
+    }
+    @Bean
+    public Modle2 getCar() {
+        return new Modle2();
+    }
+}
